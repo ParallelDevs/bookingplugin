511447ede838130672d6c4311821323996ee57ff
Alonso Mora <a.mora@paralleldevs.com>
2016-11-02 14:58:56 -0600

    Add new modals for showing messages. Remove unnecessary css and js files.

--- symfony/plugins/orangehrmBookingPlugin/modules/booking/templates/_modalsBooking.php
+++ symfony/plugins/orangehrmBookingPlugin/modules/booking/templates/_modalsBooking.php
@@ -1,15 +1,10 @@
-<?php use_stylesheet(plugin_web_path('orangehrmBookingPlugin', 'css/jquery.timepicker.css')); ?>
-<?php use_stylesheet(plugin_web_path('orangehrmBookingPlugin', 'css/addBooking.css')); ?>
-<?php use_javascript(plugin_web_path('orangehrmBookingPlugin', 'js/jquery.timepicker.min.js')); ?>
-<?php use_javascript(plugin_web_path('orangehrmBookingPlugin', 'js/addBooking.js')); ?>
-
 <div class="modal hide" id="bookingDialog">
     <div class="modal-header">
         <a class="close" data-dismiss="modal">×</a>
-        <h3><?php echo __('Add Booking') ?></h3>
+        <h3 id="bookingDialogTitle"></h3>
     </div>
     <div class="modal-body">
-        <form id="addBookingForm" method="post" action="">
+        <form id="bookingForm" method="post" action="">
             <fieldset>
                 <ol>
                     <?php echo $form->render() ?>
@@ -44,6 +39,22 @@
     </div>
 </div>
 
+<div class="modal hide" id="newBookingHolidayCollisionDialog">
+    <div class="modal-header">
+        <a class="close" data-dismiss="modal">×</a>
+        <h3><?php echo __('Collision Detected') ?></h3>
+    </div>
+    <div class="modal-body">
+        <p class="message">
+            <?= __('New booking collides with ') ?>
+            <span id="holidayCollisionName"></span>
+            <?= __('. The latter is a holiday, so there is no available time for allocating the new booking.') ?>
+        </p>
+    </div>
+    <div class="modal-footer">
+    </div>
+</div>
+
 <div class="modal hide" id="bookingAllDayCollisionDialog">
     <div class="modal-header">
         <a class="close" data-dismiss="modal">×</a>
@@ -55,6 +66,23 @@
             <?= __(' collides with ') ?>
             <span id="bookingColl2"></span>
             <?= __(', and at least one of them is an all day booking, so there is no free time for allocating the booking.') ?>
+        </p>
+    </div>
+    <div class="modal-footer">
+    </div>
+</div>
+
+<div class="modal hide" id="bookingHolidayCollisionDialog">
+    <div class="modal-header">
+        <a class="close" data-dismiss="modal">×</a>
+        <h3><?php echo __('Collision Detected') ?></h3>
+    </div>
+    <div class="modal-body">
+        <p class="message">
+            <span id="bookingColl"></span>
+            <?= __(' collides with ') ?>
+            <span id="holidayColl"></span>
+            <?= __(', and one of them is a holiday, so there is no available time for allocating the booking.') ?>
         </p>
     </div>
     <div class="modal-footer">
@@ -80,34 +108,34 @@
 <script type="text/javascript">
   jQuery(document).ready(function () {
       $("#customerId").change(function () {
-    var id = $(this).val();
-    if (id != '') {
-        $.ajax({
-      type: "POST",
-      url: '<?= url_for('@customer_projects'); ?>',
-      data: {customerId: escape(id)},
-      cache: false,
-      success: function (data)
-      {
-          fillProjectSelect('#projectId', data);
-      }
-        });
-    }
+	  var id = $(this).val();
+	  if (id != '') {
+	      $.ajax({
+		  type: "POST",
+		  url: '<?= url_for('@customer_projects'); ?>',
+		  data: {customerId: escape(id)},
+		  cache: false,
+		  success: function (data)
+		  {
+		      fillProjectSelect('#projectId', data);
+		  }
+	      });
+	  }
       });
 
       $("#dialogSave").click(function () {
-    $.ajax({
-        type: "POST",
-        url: '<?= url_for('@add_booking_ajax'); ?>',
-        data: $('#addBookingForm').serialize(),
-        cache: false,
-        success: successAddBooking,
-        dataType: "json"
-    });
+	  $.ajax({
+	      type: "POST",
+	      url: '<?= url_for('@booking_ajax'); ?>',
+	      data: $('#bookingForm').serialize(),
+	      cache: false,
+	      success: successBookingForm,
+	      dataType: "json"
+	  });
       });
 
       $("#dialogCancel").click(function () {
-    cleanAddBookingForm();
+	  cleanBookingForm();
       });
   });
 
