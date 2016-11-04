04c4ee5a9f8581645bdb4771766ba37840f3f0b1
Alonso Mora <a.mora@paralleldevs.com>
2016-11-04 11:45:04 -0600

    Change required assets in my bookings page.

--- symfony/plugins/orangehrmBookingPlugin/modules/booking/templates/viewMyBookingsSuccess.php
+++ symfony/plugins/orangehrmBookingPlugin/modules/booking/templates/viewMyBookingsSuccess.php
@@ -1,56 +1,23 @@
 <?php use_stylesheet(plugin_web_path('orangehrmBookingPlugin', 'css/fullcalendar.min.css')); ?>
 <?php use_stylesheet(plugin_web_path('orangehrmBookingPlugin', 'css/scheduler.min.css')); ?>
-<?php use_stylesheet(plugin_web_path('orangehrmBookingPlugin', 'css/bookings.css')); ?>
+<?php use_stylesheet(plugin_web_path('orangehrmBookingPlugin', 'css/booking.common.css')); ?>
+<?php use_stylesheet(plugin_web_path('orangehrmBookingPlugin', 'css/viewMyBookings.css')); ?>
 <?php use_javascript(plugin_web_path('orangehrmBookingPlugin', 'js/moment.min.js')); ?>
 <?php use_javascript(plugin_web_path('orangehrmBookingPlugin', 'js/fullcalendar.min.js')); ?>
 <?php use_javascript(plugin_web_path('orangehrmBookingPlugin', 'js/scheduler.min.js')); ?>
-<?php use_javascript(plugin_web_path('orangehrmBookingPlugin', 'js/bookings.js')); ?>
+<?php use_javascript(plugin_web_path('orangehrmBookingPlugin', 'js/booking.common.js')); ?>
+<?php use_javascript(plugin_web_path('orangehrmBookingPlugin', 'js/viewMyBookings.js')); ?>
 
 
 <script type="text/javascript">
-  $(function () { // document ready
-
-      $('#calendar').fullCalendar({
-	  schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
-	  now: '<?= date('Y-m-d') ?>',
-	  selectable: false,
-	  selectHelper: false,
-	  editable: false,
-	  aspectRatio: 4.0,
-	  firstDay: 1,
-	  header: {
-	      left: 'today prev,next',
-	      center: 'title',
-	      right: 'basicDay,agendaWeek,month'
-	  },
-	  defaultView: 'month',
-	  resources: {
-	      url: '<?= url_for('@bookables_json') ?>',
-	      data: {
-		  bookableId: <?= $bookableId ?>,
-	      },
-	      type: 'POST',
-	  },
-	  events: {
-	      url: '<?= url_for('@bookings_json') ?>',
-	      data: {
-		  bookableId: <?= $bookableId ?>,
-		  mode: 'agenda',
-	      },
-	      type: 'POST',
-	      error: errorEventHandler,
-	  },
-	  eventMouseover: eventMouseoverHandler,
-	  eventMouseout: eventMouseoutHandler,
-	  eventRender: renderEventHandler,
-	  selectAllow: false,
-	  selectOverlap: false,
-	  eventOverlap: false,
-	  eventResize: false,
-      });
-
-  });
-
+  var currentDate = '<?= date('Y-m-d') ?>';
+  var calendarMinTime = '<?= $minTime ?>';
+  var calendarMaxTime = '<?= $maxTime ?>';
+  var bookableResourcesUrl = '<?= url_for('@bookables_json') ?>';
+  var bookingResourcesUrl = '<?= url_for('@bookings_json') ?>';
+  var inactiveResourceTooltip = '<?= __('Resource is inactive') ?>';
+  var holidayLabel = '<?= __('Holiday:') ?>';
+  var bookableId = '<?= $bookableId ?>';
 </script>
 
 <div class="box" id="bookings">
