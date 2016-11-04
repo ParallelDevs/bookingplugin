bf1d01616cf40ccf898c65ce43c4f3ac9acf244a
Alonso Mora <a.mora@paralleldevs.com>
2016-11-04 13:19:53 -0600

    Load the stored configuration of the plugin.

--- symfony/plugins/orangehrmBookingPlugin/lib/form/ConfigBookingForm.php
+++ symfony/plugins/orangehrmBookingPlugin/lib/form/ConfigBookingForm.php
@@ -7,9 +7,33 @@
  */
 class ConfigBookingForm extends sfForm {
 
+  private $configBookingService;
+  private $breaksTime;
+
+  /**
+   *
+   * @param ConfigBookingService $configService
+   */
+  public function setConfigBookingService(ConfigBookingService $configService) {
+    $this->configBookingService = $configService;
+  }
+
+  /**
+   *
+   * @return type
+   */
+  public function getConfigBookingService() {
+    if (!$this->configBookingService instanceof ConfigBookingService) {
+      $this->configBookingService = new ConfigBookingService();
+      $this->configBookingService->setConfigDao(new ConfigDao());
+    }
+    return $this->configBookingService;
+  }
+
   public function configure() {
+    $this->breaksTime = $this->getConfigBookingService()->getCompanyBreaksTime();
     $this->setWidgets(array(
-      'breaksTime' => new sfWidgetFormInputText(),
+      'breaksTime' => new sfWidgetFormInputText(array(), array('value' => $this->breaksTime)),
     ));
 
     $this->setValidators(array(
