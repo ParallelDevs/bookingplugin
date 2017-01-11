5b9a590284e83f559251f79ee4df9484d2a861cc
Alonso Mora <a.mora@paralleldevs.com>
2017-01-11 17:19:47 -0600

    Adding gulp to project

--- symfony/plugins/orangehrmBookingPlugin/gulpfile.js
+++ symfony/plugins/orangehrmBookingPlugin/gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
 var config = {
-    assetsDir : './web',
+    assetsDir: '.',
     scssPattern: 'scss/**/*.scss',
     prod: !!plugins.util.env.prod,
     sourceMaps: !plugins.util.env.prod
@@ -12,26 +12,25 @@ var config = {
 
 var app = {};
 
-app.addStyles = function (paths, filename) {
+app.addStyles = function (paths, dest) {
     gulp.src(paths)
-        .pipe(plugins.plumber())
-        .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.init()))
-        .pipe(plugins.sass())
-        .pipe(plugins.concat(filename))
-        .pipe(config.prod ? plugins.cleanCss() : plugins.util.noop())
-        .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.write('.')))
-        .pipe(gulp.dest('css'));
+            .pipe(plugins.plumber())
+            .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.init()))
+            .pipe(plugins.sass())
+            .pipe(config.prod ? plugins.cleanCss() : plugins.util.noop())
+            .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.write('.')))
+            .pipe(gulp.dest(dest));
 };
 
 gulp.task('styles', function () {
     app.addStyles([
-        config.assetsDir + '/'+config.scssPattern
-    ], 'orangehrmBookingPlugin.css');
+        config.assetsDir + '/' + config.scssPattern
+    ], 'web/css');
 });
 
 
 gulp.task('watch', function () {
-    gulp.watch(config.assetsDir+'/'+config.sassPattern, ['styles']);    
+    gulp.watch(config.assetsDir + '/' + config.sassPattern, ['styles']);
 });
 
-gulp.task('default', ['styles',  'watch']);
+gulp.task('default', ['styles', 'watch']);
