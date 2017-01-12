620c51c2c81472920ca8f192bb291c5cc2cb3a26
Alonso Mora <a.mora@paralleldevs.com>
2017-01-12 17:52:59 -0600

    Adding action for getting customer projects

---
+++ symfony/plugins/orangehrmBookingPlugin/modules/booking/actions/getCustomerProjectsAction.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Description of getCustomerProjects
+ *
+ * @author amora
+ */
+class getCustomerProjectsAction extends baseBookingAction {
+
+  public function execute($request) {
+    if ($request->hasParameter('customerId')) {
+      $customerId = $request->getParameter('customerId');
+      $result = $this->getProjectService()->getProjectsByCustomerId($customerId);
+      $this->result = $result->toArray();
+      foreach ($this->result as &$project) {
+        unset($project['customerId']);
+        unset($project['is_deleted']);
+        unset($project['description']);
+      }
+    }
+    else {
+      $this->result = array();
+    }
+  }
+
+}
