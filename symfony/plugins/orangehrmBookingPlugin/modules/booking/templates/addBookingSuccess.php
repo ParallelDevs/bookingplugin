19d0b0d7932b6b32b45ed662dad0ac59f44834cb
Alonso Mora <a.mora@paralleldevs.com>
2017-02-28 13:33:44 -0600

    Updated assets for page 'add booking'.

--- symfony/plugins/orangehrmBookingPlugin/modules/booking/templates/addBookingSuccess.php
+++ symfony/plugins/orangehrmBookingPlugin/modules/booking/templates/addBookingSuccess.php
@@ -1,9 +1,5 @@
 <?php
-use_stylesheet(plugin_web_path('orangehrmBookingPlugin', 'css/jquery.datetimepicker.min.css'));
-use_stylesheet(plugin_web_path('orangehrmBookingPlugin', 'css/orangeBookingPlugin.css'));
-use_javascript(plugin_web_path('orangehrmBookingPlugin', 'js/moment.min.js'));
-use_javascript(plugin_web_path('orangehrmBookingPlugin', 'js/jquery.datetimepicker.full.min.js'));
-use_javascript(plugin_web_path('orangehrmBookingPlugin', 'js/booking.form.js'));
+use_stylesheet(plugin_web_path('orangehrmBookingPlugin', 'css/orangeBookingPlugin.min.css'));
 use_javascript(plugin_web_path('orangehrmBookingPlugin', 'js/addBooking.js'));
 
 $partialParams = array(
