b8d14f536942972575718278694c3b9824ed35da
Alonso Mora <a.mora@paralleldevs.com>
2017-02-28 13:34:09 -0600

    Updated assets for page 'add booking'.

---
+++ symfony/plugins/orangehrmBookingPlugin/src/js/addBooking.js
@@ -0,0 +1,33 @@
+//=include ../lib/moment.min.js
+//=include ../lib/jquery.datetimepicker.full.min.js
+//=include ../lib/booking.form.js
+
+jQuery(document).ready(function () {
+    $("#bookableId").change(function () {
+        var id = $(this).val();
+        if (id != '') {
+            $.ajax({
+                type: "POST",
+                url: bookableWorkShiftsUrl,
+                data: {bookableId: id},
+                cache: false,
+                success: function (data)
+                {
+                    setBookableWorkShift(data);
+                }
+            });
+        }
+    });
+
+    $("#btnSave").click(function () {
+        $("#frmBooking").submit();
+    });
+
+    if ($("#bookableId").val() !== '') {
+        $("#bookableId").change();
+    }
+
+    if ($("#customerId").val() !== '') {
+        $("#customerId").change();
+    }
+});
