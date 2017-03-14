833f4bd8b33ace91c5b8bdd761d649141d2ffb40
Alonso Mora <a.mora@paralleldevs.com>
2017-03-14 17:03:45 -0600

    Added minimun booking duration to plugin

--- symfony/plugins/orangehrmBookingPlugin/lib/form/ConfigBookingForm.php
+++ symfony/plugins/orangehrmBookingPlugin/lib/form/ConfigBookingForm.php
@@ -7,54 +7,57 @@
  */
 class ConfigBookingForm extends sfForm {
 
-  private $configBookingService;
-  private $breaksTime;
+    private $configBookingService;
+    private $breaksTime;
 
-  /**
-   *
-   * @param ConfigBookingService $configService
-   */
-  public function setConfigBookingService(ConfigBookingService $configService) {
-    $this->configBookingService = $configService;
-  }
-
-  /**
-   *
-   * @return type
-   */
-  public function getConfigBookingService() {
-    if (!$this->configBookingService instanceof ConfigBookingService) {
-      $this->configBookingService = new ConfigBookingService();
-      $this->configBookingService->setConfigDao(new ConfigDao());
+    /**
+     *
+     * @param ConfigBookingService $configService
+     */
+    public function setConfigBookingService(ConfigBookingService $configService) {
+        $this->configBookingService = $configService;
     }
-    return $this->configBookingService;
-  }
 
-  public function configure() {
-    $this->breaksTime = $this->getConfigBookingService()->getCompanyBreaksTime();
-    $this->setWidgets(array(
-      'breaksTime' => new sfWidgetFormInputText(array(), array('value' => $this->breaksTime)),
-    ));
+    /**
+     *
+     * @return type
+     */
+    public function getConfigBookingService() {
+        if (!$this->configBookingService instanceof ConfigBookingService) {
+            $this->configBookingService = new ConfigBookingService();
+            $this->configBookingService->setConfigDao(new ConfigDao());
+        }
+        return $this->configBookingService;
+    }
 
-    $this->setValidators(array(
-      'breaksTime' => new sfValidatorNumber(array('required' => true)),
-    ));
+    public function configure() {
+        $this->breaksTime = $this->getConfigBookingService()->getCompanyBreaksTime();
 
-    $formExtension = PluginFormMergeManager::instance();
-    $formExtension->mergeForms($this, 'configureBooking', 'ConfigBookingForm');
-    $this->getWidgetSchema()->setLabels($this->getFormLabels());
-  }
+        $this->setWidgets(array(
+          'breaksTime' => new sfWidgetFormInputText(array(), array('value' => $this->breaksTime, 'placeholder' => __('Breaks Time in Hours'))),
+          'minBookingTime' => new sfWidgetFormInputText(array(), array('value' => $this->breaksTime, 'placeholder' => __('Mininum Booking Duration in Hours'))),
+        ));
 
-  /**
-   *
-   * @return array
-   */
-  protected function getFormLabels() {
+        $this->setValidators(array(
+          'breaksTime' => new sfValidatorNumber(array('required' => true, 'min' => 0)),
+        ));
 
-    $labels = array(
-      'breaksTime' => __('Breaks Time'),
-    );
-    return $labels;
-  }
+        $formExtension = PluginFormMergeManager::instance();
+        $formExtension->mergeForms($this, 'configureBooking', 'ConfigBookingForm');
+        $this->getWidgetSchema()->setLabels($this->getFormLabels());
+    }
+
+    /**
+     *
+     * @return array
+     */
+    protected function getFormLabels() {
+
+        $labels = array(
+          'breaksTime' => __('Breaks Time'),
+          'minBookingTime' => __('Minimum Booking'),
+        );
+        return $labels;
+    }
 
 }
