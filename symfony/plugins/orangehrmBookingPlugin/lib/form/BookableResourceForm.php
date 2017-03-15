d5da6df2478ca689d3c34b3b6bd0455141ec9637
Alonso Mora <a.mora@paralleldevs.com>
2017-03-15 13:30:38 -0600

    Updated documentation of form. Removed unnecessary comments.

--- symfony/plugins/orangehrmBookingPlugin/lib/form/BookableResourceForm.php
+++ symfony/plugins/orangehrmBookingPlugin/lib/form/BookableResourceForm.php
@@ -7,134 +7,133 @@
  */
 class BookableResourceForm extends sfForm {
 
-  private $bookableService;
-  private $bookableResource;
-  private $bookableIsNew = true;
+    private $bookableService;
+    private $bookableResource;
+    private $bookableIsNew = true;
 
-  /**
-   *
-   * @return type
-   */
-  public function getBookableService() {
-    if (!$this->bookableService instanceof BookableResourceService) {
-      $this->bookableService = new BookableResourceService();
-      $this->bookableService->setBookableDao(new BookableResourceDao());
-    }
-    return $this->bookableService;
-  }
-
-  /**
-   *
-   * @param BookableResourceService $bookableService
-   */
-  public function setBookableService(BookableResourceService $bookableService) {
-    $this->bookableService = $bookableService;
-  }
-
-  public function configure() {
-    $bookableId = $this->getOption('bookableId');
-    if (!empty($bookableId)) {
-      $this->bookableResource = $this->getBookableService()->getBookableResourceById($bookableId);
-      $this->bookableIsNew = false;
-    }
-    else {
-      $this->bookableResource = new BookableResource();
-      $this->bookableResource->setIsActive(BookableResource::STATUS_ACTIVE);
+    /**
+     *
+     * @return type
+     */
+    public function getBookableService() {
+        if (!$this->bookableService instanceof BookableResourceService) {
+            $this->bookableService = new BookableResourceService();
+            $this->bookableService->setBookableDao(new BookableResourceDao());
+        }
+        return $this->bookableService;
     }
 
-
-    $this->setWidgets($this->getWidgets());
-    $this->setValidators($this->getValidators());
-
-    $this->setDefault('status', $this->bookableResource->getisActive());
-
-    //$formExtension = PluginFormMergeManager::instance();
-    //$formExtension->mergeForms($this, 'addBookableResource', '', 'saveBookableResource', 'BookableResourceForm');
-    $this->getWidgetSchema()->setLabels($this->getFormLabels());
-  }
-
-  /**
-   *
-   * @return \BookableResource
-   */
-  public function getBookableResource() {
-    $posts = $this->getValues();
-    try {
-      $bookable = !empty($posts['bookableId']) ? $this->getBookableService()->getBookableResourceById($posts['bookableId']) : new BookableResource();
+    /**
+     *
+     * @param BookableResourceService $bookableService
+     */
+    public function setBookableService(BookableResourceService $bookableService) {
+        $this->bookableService = $bookableService;
     }
-    catch (Exception $e) {
-      $bookable = new BookableResource();
-      sfContext::getInstance()->getLogger()->err($e->getMessage());
+
+    /**
+     * 
+     */
+    public function configure() {
+        $bookableId = $this->getOption('bookableId');
+        if (!empty($bookableId)) {
+            $this->bookableResource = $this->getBookableService()->getBookableResourceById($bookableId);
+            $this->bookableIsNew = false;
+        }
+        else {
+            $this->bookableResource = new BookableResource();
+            $this->bookableResource->setIsActive(BookableResource::STATUS_ACTIVE);
+        }
+
+        $this->setWidgets($this->getWidgets());
+        $this->setValidators($this->getValidators());
+
+        $this->setDefault('status', $this->bookableResource->getisActive());
+
+        $this->getWidgetSchema()->setLabels($this->getFormLabels());
     }
-    $bookable->setEmpNumber($posts['empNum']);
-    $bookable->setIsActive($posts['status']);
-    $bookable->setBookableColor($posts['bookableColor']);
-    return $bookable;
-  }
 
-  /**
-   *
-   * @return type
-   */
-  public function save() {
-    $bookable = $this->getBookableResource();
-    $service = $this->getBookableService();
-    $service->saveBookableResource($bookable);
-    $bookableId = $bookable->bookableId;
-    return $bookableId;
-  }
+    /**
+     *
+     * @return \BookableResource
+     */
+    public function getBookableResource() {
+        $posts = $this->getValues();
+        try {
+            $bookable = !empty($posts['bookableId']) ? $this->getBookableService()->getBookableResourceById($posts['bookableId']) : new BookableResource();
+        }
+        catch (Exception $e) {
+            $bookable = new BookableResource();
+            sfContext::getInstance()->getLogger()->err($e->getMessage());
+        }
+        $bookable->setEmpNumber($posts['empNum']);
+        $bookable->setIsActive($posts['status']);
+        $bookable->setBookableColor($posts['bookableColor']);
+        return $bookable;
+    }
 
-  /**
-   *
-   * @return type
-   */
-  protected function getWidgets() {
-    $status = array(
-      BookableResource::STATUS_ACTIVE => __('Active'),
-      BookableResource::STATUS_INACTIVE => __('Inactive'),
-    );
+    /**
+     *
+     * @return type
+     */
+    public function save() {
+        $bookable = $this->getBookableResource();
+        $service = $this->getBookableService();
+        $service->saveBookableResource($bookable);
+        $bookableId = $bookable->bookableId;
+        return $bookableId;
+    }
 
-    $widgets = array(
-      'bookableId' => new sfWidgetFormInputHidden(array(), array('value' => $this->bookableResource->getBookableId())),
-      'employee' => 
-      $this->bookableIsNew ? 
-        new ohrmWidgetEmployeeNameAutoFill(array('loadingMethod' => 'ajax'))
-        : new sfWidgetFormInputText(array(), array('value'=> $this->bookableResource->getEmployeeName(),'readonly'=>true, "class"=>"read-only")),
-      'empNum' => new sfWidgetFormInputHidden(array(), array('value' => $this->bookableResource->getEmpNumber())),
-      'status' => new sfWidgetFormSelect(array('choices' => $status), array('value' => $this->bookableResource->getisActive(), "class" => "formInputText editable")),
-      'bookableColor' => new sfWidgetFormInputText(array(), array('value' => $this->bookableResource->getBookableColor(), 'class' => 'editable')),
-    );
+    /**
+     *
+     * @return type
+     */
+    protected function getWidgets() {
+        $status = array(
+          BookableResource::STATUS_ACTIVE => __('Active'),
+          BookableResource::STATUS_INACTIVE => __('Inactive'),
+        );
 
-    return $widgets;
-  }
+        $widgets = array(
+          'bookableId' => new sfWidgetFormInputHidden(array(), array('value' => $this->bookableResource->getBookableId())),
+          'employee' =>
+          $this->bookableIsNew ?
+          new ohrmWidgetEmployeeNameAutoFill(array('loadingMethod' => 'ajax')) : new sfWidgetFormInputText(array(), array('value' => $this->bookableResource->getEmployeeName(), 'readonly' => true, "class" => "read-only")),
+          'empNum' => new sfWidgetFormInputHidden(array(), array('value' => $this->bookableResource->getEmpNumber())),
+          'status' => new sfWidgetFormSelect(array('choices' => $status), array('value' => $this->bookableResource->getisActive(), "class" => "formInputText editable")),
+          'bookableColor' => new sfWidgetFormInputText(array(), array('value' => $this->bookableResource->getBookableColor(), 'class' => 'editable')),
+        );
 
-  /**
-   *
-   * @return type
-   */
-  protected function getValidators() {
-    $validators = array(
-      'bookableId' => new sfValidatorDoctrineChoice(array('model' => 'BookableResource', 'required' => false)),
-      'employee' => new sfValidatorString(array('required' => false)),
-      'empNum' => new sfValidatorString(array('required' => false)),
-      'status' => new sfValidatorString(array('required' => false)),
-      'bookableColor' => new sfValidatorString(array('required' => false)),
-    );
-    return $validators;
-  }
+        return $widgets;
+    }
 
-  /**
-   *
-   * @return array
-   */
-  protected function getFormLabels() {
+    /**
+     *
+     * @return type
+     */
+    protected function getValidators() {
+        $validators = array(
+          'bookableId' => new sfValidatorDoctrineChoice(array('model' => 'BookableResource', 'required' => false)),
+          'employee' => new sfValidatorString(array('required' => false)),
+          'empNum' => new sfValidatorString(array('required' => false)),
+          'status' => new sfValidatorString(array('required' => false)),
+          'bookableColor' => new sfValidatorString(array('required' => false)),
+        );
+        return $validators;
+    }
 
-    $labels = array(
-      'employee' => __('Employee'),
-      'status' => __('Status'),
-      'bookableColor' => __('Color'),
-    );
-    return $labels;
-  }
+    /**
+     *
+     * @return array
+     */
+    protected function getFormLabels() {
+
+        $labels = array(
+          'employee' => __('Employee'),
+          'status' => __('Status'),
+          'bookableColor' => __('Color'),
+        );
+        return $labels;
+    }
 
 }
