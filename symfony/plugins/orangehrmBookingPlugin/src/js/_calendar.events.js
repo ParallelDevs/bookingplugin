c41be691680edc99b83cafa16357864a8151bc2e
Alonso Mora <a.mora@paralleldevs.com>
2017-03-21 14:27:21 -0600

    Added render of event in week view

--- symfony/plugins/orangehrmBookingPlugin/src/js/_calendar.events.js
+++ symfony/plugins/orangehrmBookingPlugin/src/js/_calendar.events.js
@@ -10,14 +10,17 @@ function eventRender(event, element, view) {
         element.addClass('fc-nonbusiness booking-holiday');
     } else {
         element.tipTip({
-                content: event.customerName + ' - ' + event.title
+            content: event.customerName + ' - ' + event.title
         });
         if (view) {
             switch (view.type) {
                 case "timelineMonth":
                     element.find(".fc-time").text(event.duration + 'h p/d');
                     element.find(".fc-title").remove();
-                    break;                
+                    break;
+                case "timelineWeek":
+                    element.find(".fc-title").text(event.duration + 'h p/d');
+                    break;
                 default:
                     break;
             }
