fc305fda41dd3cee48643c1608db7a697af2365e
Alonso Mora <a.mora@paralleldevs.com>
2017-03-21 08:52:48 -0600

    Compiled JS of bookable resources

--- symfony/plugins/orangehrmBookingPlugin/web/js/addBookableResource.js
+++ symfony/plugins/orangehrmBookingPlugin/web/js/addBookableResource.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("#btnSave").click(function(){$("#empNum").val($("#employee_empId").val()),$("#frmBookable").submit()}),$("#bookableColor").spectrum({preferredFormat:"hex"})});
+$(document).ready(function(){$("#btnSave").click(function(){$("#empNum").val($("#employee_empId").val()),$("#frmBookable").submit()})});
