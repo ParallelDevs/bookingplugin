792ed26cac79b21f2d7f417f2822fe5bfd71397b
Alonso Mora <a.mora@paralleldevs.com>
2017-03-23 16:46:37 -0600

    Added modal for editing a booking. Refactor of buttons

--- symfony/plugins/orangehrmBookingPlugin/modules/booking/templates/_bookingModals.php
+++ symfony/plugins/orangehrmBookingPlugin/modules/booking/templates/_bookingModals.php
@@ -1,13 +1,25 @@
 <div id="addBooking" class="modal hide">
     <div class="modal-header">
         <a class="close" data-dismiss="modal">×</a>
-        <h3 id="bookingDialogTitle"><?= __('Add Booking') ?></h3>
+        <h3 class="booking-dialog-title"><?= __('Add Booking') ?></h3>
     </div>
     <div class="modal-body"></div>
     <div class="modal-footer">
-        <input type="button" id="modal-add-save" name="dialogSave" class="btn" value="<?php echo __('Save'); ?>" />
-        <input type="button"  id="modal-add-cancel" name="dialogCancel" class="btn reset" data-dismiss="modal"
-               value="<?php echo __('Cancel'); ?>" />
+        <input type="button" name="dialogSave" class="btn btn-booking btn-save" value="<?= __('Save'); ?>" />
+        <input type="button" name="dialogCancel" class="btn reset" data-dismiss="modal"
+               value="<?= __('Cancel'); ?>" />
     </div>
 </div>
 
+<div id="editBooking" class="modal hide">
+    <div class="modal-header">
+        <a class="close" data-dismiss="modal">×</a>
+        <h3 class="booking-dialog-title"><?= __('Edit Booking') ?></h3>
+    </div>
+    <div class="modal-body"></div>
+    <div class="modal-footer">
+        <input type="button" name="dialogSave" class="btn btn-booking btn-save" value="<?= __('Save'); ?>" />
+        <input type="button" name="dialogCancel" class="btn reset" data-dismiss="modal"
+               value="<?= __('Cancel'); ?>" />
+    </div>
+</div>
