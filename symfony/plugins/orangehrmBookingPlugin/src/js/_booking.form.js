3c66c3445cbc75867f79711be795f1f10360e7f8
Alonso Mora <a.mora@paralleldevs.com>
2017-03-23 15:08:01 -0600

    Refactor of js behaviors of booking form

--- symfony/plugins/orangehrmBookingPlugin/src/js/_booking.form.js
+++ symfony/plugins/orangehrmBookingPlugin/src/js/_booking.form.js
@@ -7,127 +7,34 @@ function fillProjectSelect(id, data) {
     });
 }
 
-function lockForm() {
-    $("#startDate").attr('disabled', 'disabled');
-    $("#endDate").attr('disabled', 'disabled');
-    $("#startTime").attr('disabled', 'disabled');
-    $("#endTime").attr('disabled', 'disabled');
-    $('#hours').attr('disabled', 'disabled');
-    $('#minutes').attr('disabled', 'disabled');
-    $('#customerId').attr('disabled', 'disabled');
-    $('#projectId').attr('disabled', 'disabled');
-}
-
-function unlockForm() {
-    $("#startDate").removeAttr('disabled');
-    $("#endDate").removeAttr('disabled');
-    $("#startTime").removeAttr('disabled');
-    $("#endTime").removeAttr('disabled');
-    $('#hours').removeAttr('disabled');
-    $('#minutes').removeAttr('disabled');
-    $('#customerId').removeAttr('disabled');
-    $('#projectId').removeAttr('disabled');
-}
-
-function setBookableWorkShift(data) {
-    var momentMinTime = moment(data.minTime, 'H:m');
-    var momentMaxTime = moment(data.maxTime, 'H:m');
-
-    jQuery('#minStartTime').val(momentMinTime.format('HH:mm:ss'));
-    jQuery('#maxEndTime').val(momentMaxTime.format('HH:mm:ss'));
-
-    momentMaxTime.add(15, 'minutes');
-
-    jQuery('#startTime').datetimepicker({
-        minTime: momentMinTime.toDate(),
-        maxTime: momentMaxTime.toDate()
-    });
-
-    jQuery('#endTime').datetimepicker({
-        minTime: momentMinTime.toDate(),
-        maxTime: momentMaxTime.toDate()
-    });
-
-}
-
-
-jQuery(document).ready(function () {
-
-    var id = $("#bookableId").val();
-    if (id === '') {
-        lockForm();
+function customerChangeHandler() {
+    var id = $(this).val();
+    if (id !== '') {
+        $.ajax({
+            type: "POST",
+            url: customerProjectUrl,
+            data: {customerId: id},
+            cache: false,
+            success: function (data)
+            {
+                fillProjectSelect('#projectId', data);
+            }
+        });
     }
+}
 
-    $("#bookableId").change(function () {
-        var id = $(this).val();
-        if (id !== '') {
-            $.ajax({
-                type: "POST",
-                url: bookableWorkShiftsUrl,
-                data: {bookableId: id},
-                cache: false,
-                success: function (data)
-                {
-                    unlockForm();
-                    setBookableWorkShift(data);
-                }
-            });
-        }
-    });
+function startDateChangeHandler(startDateId, endDateId) {
+    var startDate = $(startDateId).val();
+    if ($(endDateId).val() === '') {
+        $(endDateId).val(startDate);
+    }
+}
 
-    $("#customerId").change(function () {
-        var id = $(this).val();
-        if (id !== '') {
-            $.ajax({
-                type: "POST",
-                url: customerProjectUrl,
-                data: {customerId: id},
-                cache: false,
-                success: function (data)
-                {
-                    fillProjectSelect('#projectId', data);
-                }
-            });
-        }
-    });
-
-    $("#startDate").change(function () {
-        var startDate = $(this).val();
-        if ($("#endDate").val() === '') {
-            $("#endDate").val(startDate);
-        }
-    });
-
-    jQuery('#startDate').datetimepicker({
+function initDateField(fieldId) {
+    jQuery(fieldId).datetimepicker({
         timepicker: false,
         format: 'Y-m-d',
         formatDate: 'Y-m-d',
         dayOfWeekStart: firstDayOfWeek
     });
-
-    jQuery('#endDate').datetimepicker({
-        timepicker: false,
-        format: 'Y-m-d',
-        formatDate: 'Y-m-d',
-        dayOfWeekStart: firstDayOfWeek
-    });
-
-    jQuery('#startTime').datetimepicker({
-        datepicker: false,
-        timepicker: true,
-        format: 'H:i',
-        formatTime: 'H:i',
-        step: 15,
-        dayOfWeekStart: firstDayOfWeek
-    });
-
-    jQuery('#endTime').datetimepicker({
-        datepicker: false,
-        timepicker: true,
-        format: 'H:i',
-        formatTime: 'H:i',
-        step: 15,
-        dayOfWeekStart: firstDayOfWeek
-    });
-
-});
+}
