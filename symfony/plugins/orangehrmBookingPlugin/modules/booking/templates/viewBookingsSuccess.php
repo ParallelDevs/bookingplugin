e98b30addcf867df194bc1bd85bf08354e4411b7
Alonso Mora <a.mora@paralleldevs.com>
2017-04-20 08:28:14 -0600

    Updated  page 'view bookings'

--- symfony/plugins/orangehrmBookingPlugin/modules/booking/templates/viewBookingsSuccess.php
+++ symfony/plugins/orangehrmBookingPlugin/modules/booking/templates/viewBookingsSuccess.php
@@ -11,29 +11,31 @@ use_javascript(plugin_web_path('orangehrmBookingPlugin', 'js/viewBookings.js'));
   var calendarMinTime = '<?= $minTime ?>';
   var calendarMaxTime = '<?= $maxTime ?>';
   var firstDayOfWeek = <?= $firstDayOfWeek; ?>;
-  var inactiveResourceTooltip = '<?= __('Resource is inactive') ?>';
-  var holidayLabel = '<?= __('Holiday:') ?>';
+  var inactiveResourceTooltip = '<?= __("Resource is inactive") ?>';
+  var holidayLabel = '<?= __("Holiday:") ?>';
   var bookableResourceTitle = '<?= __("Resources") ?>';
+  var filterBtnLabel = '<?= __("Filter") ?>';
   var confirmBookingNonBusiness = '<?= __("Are you sure you want a booking in a non business day?") ?>';
   var confirmStartBookingNonBusiness = '<?= __("Are you sure you want to start a booking in a non business day?") ?>';
   var confirmEndBookingNonBusiness = '<?= __("Are you sure you want to end a booking in a non business day?") ?>';
   var confirmBookingHoliday = '<?= __("Are you sure you want a booking during a holiday?") ?>';
   var confirmDeleteBooking = '<?= __("Are you sure you want to delete this booking? This action cannot be undone.") ?>'
-  var bookableResourcesUrl = '<?= url_for('@bookables_json') ?>';
-  var bookingResourcesUrl = '<?= url_for('@bookings_json') ?>';
-  var customerProjectUrl = '<?= url_for('@customer_projects'); ?>';
-  var bookableWorkShiftsUrl = '<?= url_for('@bookable_workshifts'); ?>';
-  var bookingFormUrl = '<?= url_for('@form_booking') ?>';
-  var saveBookingUrl = '<?= url_for('@save_booking'); ?>';
-  var deleteBookingUrl = '<?= url_for('@delete_booking'); ?>';
+  var bookableResourcesUrl = '<?= url_for("@bookables_json") ?>';
+  var bookingResourcesUrl = '<?= url_for("@bookings_json") ?>';
+  var customerProjectUrl = '<?= url_for("@customer_projects"); ?>';
+  var bookableWorkShiftsUrl = '<?= url_for("@bookable_workshifts"); ?>';
+  var bookingFormUrl = '<?= url_for("@form_booking") ?>';
+  var saveBookingUrl = '<?= url_for("@save_booking"); ?>';
+  var deleteBookingUrl = '<?= url_for("@delete_booking"); ?>';
 </script>
 
 <div class="box" id="bookings">
     <div class="head">
         <h1><?php echo __("Bookings") ?></h1>
     </div>
-    <div class="inner">
-        <div id='loading'></div>
+    <div class="inner">        
+        <input type="text" id="searchStartDate" name="searchStartDate"/>
+        <input type="text" id="searchEndDate" name="searchEndDate"/>
         <div id='calendar'></div>
     </div>
 </div>
--- symfony/plugins/orangehrmBookingPlugin/src/js/viewBookings.js
+++ symfony/plugins/orangehrmBookingPlugin/src/js/viewBookings.js
@@ -5,8 +5,22 @@ $(function () {
     calendarOptions.firstDay = firstDayOfWeek;
     calendarOptions.minTime = calendarMinTime;
     calendarOptions.maxTime = calendarMaxTime;
+    calendarOptions.resourceLabelText= bookableResourceTitle;
+    calendarOptions.customButtons.filter.text = filterBtnLabel;
     calendarOptions.resources.url = bookableResourcesUrl;   
     calendarOptions.events.url = bookingResourcesUrl;    
     
     $('#calendar').fullCalendar(calendarOptions);
+    jQuery("#searchStartDate").datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d',
+        formatDate: 'Y-m-d',
+        dayOfWeekStart: firstDayOfWeek        
+    });
+    jQuery("#searchEndDate").datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d',
+        formatDate: 'Y-m-d',
+        dayOfWeekStart: firstDayOfWeek        
+    });
 });
