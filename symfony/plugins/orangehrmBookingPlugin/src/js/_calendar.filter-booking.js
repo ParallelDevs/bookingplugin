a442a78be09f9358c79e909f05bd9ffb69c96643
Alonso Mora <a.mora@paralleldevs.com>
2017-04-20 15:42:26 -0600

    Updated filter by date range

--- symfony/plugins/orangehrmBookingPlugin/src/js/_calendar.filter-booking.js
+++ symfony/plugins/orangehrmBookingPlugin/src/js/_calendar.filter-booking.js
@@ -1,30 +1,54 @@
 function filterBookings() {
-    var startDate = $("#searchStartDate").val();
-    var endDate = $("#searchEndDate").val();
+    var startDate = moment($("#searchStartDate").val(), "YYYY-MM-DD", true);
+    var endDate = moment($("#searchEndDate").val(), "YYYY-MM-DD", true);
 
-    if (!jQuery.isEmptyObject(startDate) && !jQuery.isEmptyObject(endDate)) {
-        startDate = moment($("#searchStartDate").val(), 'YYYY-MM-DD');
-        endDate = moment($("#searchEndDate").val(), 'YYYY-MM-DD');
-        var diffDays = endDate.add(1, 'days').diff(startDate, 'days');
+    if (startDate.isValid() && endDate.isValid() && endDate.isSameOrAfter(startDate)) {
+        var diffDays = endDate.add(1, 'days')
+                .diff(startDate, 'days');
         calendarOptions.views.filterDates.duration.days = diffDays;
-        $('#calendar').fullCalendar('destroy');
-        $('#calendar').fullCalendar(calendarOptions);
-        $('#calendar').fullCalendar('changeView', 'filterDates', startDate);
+        $("#calendar").fullCalendar('destroy');
+        $("#calendar").fullCalendar(calendarOptions);
+        $("#calendar").fullCalendar('changeView', "filterDates");
+        $("#calendar").fullCalendar('gotoDate', startDate);
     }
-
 }
 
-$(document).ready(function(){
+function changeSearchStartDate() {
+    var startDate = moment($(this).val(), "YYYY-MM-DD", true);
+    var currentView = $('#calendar').fullCalendar('getView');
+    var endDate = '';
+    if ($("#searchEndDate").val() === '') {
+        switch (currentView.name) {
+            case 'timelineMonth':
+                endDate = startDate.add(1, "month")
+                        .format("YYYY-MM-DD");
+                break;
+            case 'timelineWeek':
+                endDate = startDate.add(1, "week")
+                        .format("YYYY-MM-DD");
+                break;
+            default:
+                endDate = startDate.format("YYYY-MM-DD");
+                break;
+        }
+        $("#searchEndDate").val(endDate);
+    }
+}
+
+$(document).ready(function () {
     jQuery("#searchStartDate").datetimepicker({
         timepicker: false,
-        format: 'Y-m-d',
-        formatDate: 'Y-m-d',
+        format: "Y-m-d",
+        formatDate: "Y-m-d",
         dayOfWeekStart: firstDayOfWeek
     });
+
     jQuery("#searchEndDate").datetimepicker({
         timepicker: false,
-        format: 'Y-m-d',
-        formatDate: 'Y-m-d',
+        format: "Y-m-d",
+        formatDate: "Y-m-d",
         dayOfWeekStart: firstDayOfWeek
     });
+
+    $("#searchStartDate").change(changeSearchStartDate);
 });
