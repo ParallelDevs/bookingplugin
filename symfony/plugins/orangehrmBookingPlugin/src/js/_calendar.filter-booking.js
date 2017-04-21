f164d277135f87ae8c8c5319b8e515405218df92
Alonso Mora <a.mora@paralleldevs.com>
2017-04-21 09:09:39 -0600

    Updated filtering of bookings

--- symfony/plugins/orangehrmBookingPlugin/src/js/_calendar.filter-booking.js
+++ symfony/plugins/orangehrmBookingPlugin/src/js/_calendar.filter-booking.js
@@ -1,14 +1,14 @@
-function filterBookings() {
+function filterBookings(viewFilter) {
     var startDate = moment($("#searchStartDate").val(), "YYYY-MM-DD", true);
     var endDate = moment($("#searchEndDate").val(), "YYYY-MM-DD", true);
 
     if (startDate.isValid() && endDate.isValid() && endDate.isSameOrAfter(startDate)) {
         var diffDays = endDate.add(1, 'days')
                 .diff(startDate, 'days');
-        calendarOptions.views.filterDates.duration.days = diffDays;
+        calendarOptions.views[viewFilter].duration.days = diffDays;
         $("#calendar").fullCalendar('destroy');
         $("#calendar").fullCalendar(calendarOptions);
-        $("#calendar").fullCalendar('changeView', "filterDates",startDate);
+        $("#calendar").fullCalendar('changeView', viewFilter,startDate);
     }
 }
 
