5164a805507f80e6a28aa0ee345b0fd436ae87c9
Alonso Mora <a.mora@paralleldevs.com>
2017-10-06 11:35:55 -0600

    Updated default business hours

--- symfony/plugins/orangehrmBookingPlugin/modules/booking/actions/viewMyBookingsAction.class.php
+++ symfony/plugins/orangehrmBookingPlugin/modules/booking/actions/viewMyBookingsAction.class.php
@@ -28,8 +28,8 @@ class viewMyBookingsAction extends baseBookingAction {
 
     $limitHours = BusinessBookingPluginService::getCompanyBusinessLimitHoursForCalendar();
     $firstDay = BusinessBookingPluginService::getCompanyFirstBusinessDay();
-    $this->minTime = (!empty($limitHours) && isset($limitHours['minHour'])) ? $limitHours['minHour'] : "00:00:00";
-    $this->maxTime = (!empty($limitHours) && isset($limitHours['maxHour'])) ? $limitHours['maxHour'] : "24:00:00";
+    $this->minTime = (!empty($limitHours) && isset($limitHours['minHour'])) ? $limitHours['minHour'] : "09:00:00";
+    $this->maxTime = (!empty($limitHours) && isset($limitHours['maxHour'])) ? $limitHours['maxHour'] : "18:00:00";
     $this->firstDayOfWeek = $firstDay;
   }
 
