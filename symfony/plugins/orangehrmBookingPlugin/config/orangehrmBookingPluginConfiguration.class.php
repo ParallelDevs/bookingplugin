17c1ed60311db91bfcdc2b7d2f239d38c0a3c2dc
Alonso Mora <a.mora@paralleldevs.com>
2017-10-20 10:24:45 -0600

    Added listener to plugin events

--- symfony/plugins/orangehrmBookingPlugin/config/orangehrmBookingPluginConfiguration.class.php
+++ symfony/plugins/orangehrmBookingPlugin/config/orangehrmBookingPluginConfiguration.class.php
@@ -7,11 +7,18 @@
  */
 class orangehrmBookingPluginConfiguration extends sfPluginConfiguration {
 
-	public function initialize() {
-		$enabledModules = sfConfig::get('sf_enabled_modules');
-		if (is_array($enabledModules)) {
-			sfConfig::set('sf_enabled_modules', array_merge(sfConfig::get('sf_enabled_modules'), array('booking')));
-		}
-	}
+  protected static $eventsBound = false;
+
+  public function initialize() {
+    $enabledModules = sfConfig::get('sf_enabled_modules');
+    if (is_array($enabledModules)) {
+      sfConfig::set('sf_enabled_modules', array_merge(sfConfig::get('sf_enabled_modules'), array('booking')));
+    }
+    
+    if (!self::$eventsBound) {
+      $this->dispatcher->connect(BookingEvents::BOOKING_SAVE, array(new BookingMailer(), 'listen'));
+      self::$eventsBound = true;
+    }
+  }
 
 }
