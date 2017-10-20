ecca3205e6297215592b2a30e3ddc2f025c5485b
Alonso Mora <a.mora@paralleldevs.com>
2017-10-20 10:20:56 -0600

    Added classes for handling events of plugin

---
+++ symfony/plugins/orangehrmBookingPlugin/lib/event/BookingEvents.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of BookingEvents
+ *
+ * @author pdev
+ */
+class BookingEvents {
+  const BOOKING_SAVE = 'booking.save';  
+}
