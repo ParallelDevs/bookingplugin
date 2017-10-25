40fcef89a2eaeef0aa942da64cdc98fcb5f57b6f
Alonso Mora <a.mora@paralleldevs.com>
2017-10-25 10:59:30 -0600

    Added notification after deleting a booking

--- symfony/plugins/orangehrmBookingPlugin/modules/booking/actions/deleteBookingAction.class.php
+++ symfony/plugins/orangehrmBookingPlugin/modules/booking/actions/deleteBookingAction.class.php
@@ -16,19 +16,51 @@ class deleteBookingAction extends baseBookingAction {
     if ($this->bookingPermissions->canDelete()) {
       $response = array();
 
-      $bookingId = $request->getParameter('bookingId');
+      if ($request->isMethod('post')) {
+        $this->form = new BookingForm(array(), array(), true);
+        $this->form->bind($request->getPostParameters(), $request->getFiles());
+        $this->deleteBooking($response);
+      }
+
+      $this->result = $response;
+    }
+  }
+
+  /**
+   *
+   * @param type $response
+   */
+  private function deleteBooking(&$response) {
+    if ($this->form->isValid()) {
+      $bookingId = $this->form->getValue('bookingId');
       try {
         $this->getBookingService()->deleteBooking($bookingId);
         $response['success'] = true;
         $response['errors'] = array();
+        $this->sendNotification();
       }
       catch (Exception $e) {
         $response['success'] = false;
         $response['errors'] = array($e->getMessage());
         sfContext::getInstance()->getLogger()->err($e->getMessage());
       }
+    }
+  }
 
-      $this->result = $response;
+  /**
+   *
+   */
+  private function sendNotification() {
+    $notify = $this->form->getValue('notify');
+
+    if (!empty($notify)) {
+      $eventType = BookingEvents::BOOKING_DELETE;
+      $eventData = array(
+        'bookableId' => $this->form->getValue('bookableId'),
+        'projectId' => $this->form->getValue('projectId'),
+        'actionType' => $eventType,
+      );
+      $this->getDispatcher()->notify(new sfEvent($this, $eventType, $eventData));
     }
   }
 
