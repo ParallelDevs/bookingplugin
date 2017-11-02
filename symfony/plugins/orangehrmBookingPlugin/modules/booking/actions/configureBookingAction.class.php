9d7725d105fac712297f028373a87affac48c3fc
Alonso Mora <a.mora@paralleldevs.com>
2017-11-02 11:33:20 -0600

    Refactor de configure booking action class

--- symfony/plugins/orangehrmBookingPlugin/modules/booking/actions/configureBookingAction.class.php
+++ symfony/plugins/orangehrmBookingPlugin/modules/booking/actions/configureBookingAction.class.php
@@ -7,28 +7,6 @@
  */
 class configureBookingAction extends baseBookingAction {
 
-  private $configBookingService;
-
-  /**
-   *
-   * @param ConfigBookingService $configService
-   */
-  public function setConfigBookingService(ConfigBookingService $configService) {
-    $this->configBookingService = $configService;
-  }
-
-  /**
-   *
-   * @return type
-   */
-  public function getConfigBookingService() {
-    if (!$this->configBookingService instanceof ConfigBookingService) {
-      $this->configBookingService = new ConfigBookingService();
-      $this->configBookingService->setConfigDao(new ConfigDao());
-    }
-    return $this->configBookingService;
-  }
-
   /**
    *
    * @param sfForm $form
@@ -64,9 +42,9 @@ class configureBookingAction extends baseBookingAction {
       if ($this->form->isValid()) {
 
         $breaks = $this->form->getValue('breaksTime');
-        
+
         try {
-          $this->getConfigBookingService()->setCompanyBreaksTime($breaks);          
+          $this->getConfigBookingService()->setCompanyBreaksTime($breaks);
           $this->getUser()->setFlash('success', __(TopLevelMessages::SAVE_SUCCESS));
           $this->redirect('booking/configureBooking');
         }
