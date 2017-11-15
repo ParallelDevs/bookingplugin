c2114c0fe101dad7a2a0cd9d08d48d8dc5b66009
Alonso Mora <a.mora@paralleldevs.com>
2017-11-15 17:04:58 -0600

    Added license form

---
+++ symfony/plugins/orangehrmBookingPlugin/lib/form/LicenseBookingForm.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Description of ConfigureBookingForm
+ *
+ * @author alonso
+ */
+class LicenseBookingForm extends sfForm {
+
+  private $configBookingService;
+
+  /**
+   *
+   * @param ConfigBookingService $configService
+   */
+  public function setConfigBookingService(ConfigBookingService $configService) {
+    $this->configBookingService = $configService;
+  }
+
+  /**
+   *
+   * @return type
+   */
+  public function getConfigBookingService() {
+    if (!$this->configBookingService instanceof ConfigBookingService) {
+      $this->configBookingService = new ConfigBookingService();
+      $this->configBookingService->setConfigDao(new ConfigDao());
+    }
+    return $this->configBookingService;
+  }
+
+  public function configure() {
+    $this->setWidgets(array(
+      'email' => new sfWidgetFormInputText(array(), array('placeholder' => __('Email'))),
+      'licenseKey' => new sfWidgetFormInputText(array(), array('placeholder' => __('License Key'))),
+    ));
+
+    $this->setDefaults(array(
+      'email' => $this->getConfigBookingService()->getLicenseEmail(),
+      'licenseKey' => $this->getConfigBookingService()->getLicenseKey(),
+    ));
+
+    $this->setValidators(array(
+      'email' => new sfValidatorEmail(array('required' => true)),
+      'licenseKey' => new sfValidatorString(array('required' => true)),
+    ));
+
+    $formExtension = PluginFormMergeManager::instance();
+    $formExtension->mergeForms($this, 'activateBooking', 'ActivateBookingForm');
+    $this->getWidgetSchema()->setLabels($this->getFormLabels());
+  }
+
+  /**
+   *
+   * @return array
+   */
+  protected function getFormLabels() {
+
+    $labels = array(
+      'email' => __('Email'),
+      'licenseKey' => __('License Key'),
+    );
+    return $labels;
+  }
+
+}
